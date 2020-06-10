36bbcdf7fb49b688e79ef8d2d8e9ef324b6639b0
Sagie <Sagie012@gmail.com>
2020-06-11 00:48:59 +0300

    Add message model

---
+++ src/models/message.ts
@@ -0,0 +1,25 @@
+import * as mongoose from 'mongoose';
+import { Schema } from 'mongoose';
+
+const messageSchema = new Schema({
+  sourceUser: { type: Schema.Types.ObjectId, ref: "user", required: true },
+  destUser: { type: Schema.Types.ObjectId, ref: "user", required: true },
+  title: { type: String, require: true },
+  content: { type: String, required: true }
+});
+
+export const messagesModel = mongoose.model('message', messageSchema);
+
+export function getMessagesByUsername(username): Promise<any> {
+  let query = { destUser: username };
+  return messagesModel.find(query).populate('sourceUser').populate('destUser').exec();
+}
+
+export async function totalMessagesAmount(username): Promise<number> {
+  let messages = await getMessagesByUsername(username);
+  return messages.map(msg => 1).reduce((sum, currentMsg) => sum + currentMsg);
+}
+
+export function getAllMessages() {
+  return messagesModel.find().populate('sourceUser').populate('destUser').exec();
+}
